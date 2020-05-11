5d46bcbd89c8cce94a6650c7dc6e04d7ac3631e3
Juan José Valencia <juanchouni@hotmail.com>
2020-05-11 18:59:25 -0500

    gui project package added

---
+++ code/project_gui/ConsolidatedPage.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace project_gui
+{
+    partial class ConsolidatedPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ConsolidatedPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ConsolidatedPage";
+            this.Text = "ConsolidatedPage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
