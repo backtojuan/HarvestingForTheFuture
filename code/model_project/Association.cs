5e77101f1196a13c28398bd58277f8a49878cdff
Natalia Gonzalez <nataliaisagonzalez26@gmail.com>
2020-05-11 14:50:50 -0500

    model_project package created

---
+++ code/model_project/Association.cs
@@ -0,0 +1,26 @@
+﻿using project_model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_project
+{
+    class Association
+    {
+        private Zone zone;
+        private List<Farming> farmings;
+        private List<Relation> relations;
+
+        public Association()
+        {
+
+        }
+
+        public Zone GetZone { get => zone; set => zone = value; }
+        public List<Farming> GetAreas { get => farmings; set => farmings = value; }
+        public List<Relation> GetRelations { get => relations; set => relations = value; }
+
+    }
+}
---
+++ code/model_project/Project.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_model
+{
+    class Project
+    {
+
+        public static String DEPARTMENTS_ROUTE = "";
+        public static String DESCRIPTIONS_ROUTE = "";
+
+        public Project()
+        {
+
+        }
+
+        public Boolean ActualizeDB()
+        {
+            return true;
+        }
+
+        public void Association(String department)
+        {
+
+        }
+
+
+
+
+    }
+}
