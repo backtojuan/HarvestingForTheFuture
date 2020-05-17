b1f183a1e3cbb3e30ebb37202953e39c1b2e3e28
Lina Salinas <linasalinasd7@gmail.com>
2020-05-16 19:46:23 -0500

    The DataFirstPage package has been totally modified, some classes have been removed and others have been modified, the method that takes information from a plain text file has been created.

--- code/DataFistPage/references.cs
+++ code/DataFistPage/references.cs
@@ -3,22 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DataFistPage
 {
     class References
     {
-        private DataFistPage.Images image = new Images();
-        private DataFistPage.ListDepartament listDepartament = new ListDepartament();
-        private DataFistPage.Description description = new Description();
+        private String name;
+        private String description;
+        private String image;
+        const String DEPARTAMENT = "..\\..\\..\\..\\code\\Data\\DATA_FIRST_PAGE\\Departament.txt";
+        const String CULTIVO = "..\\..\\..\\..\\code\\Data\\DATA_FIRST_PAGE\\Cultivo.txt";
 
-       /* public String assignation(Images image, Description description, ListDepartament listDepartament)
+       private List<String> departament = new List<String>();
+        public void Read()
         {
-            if (Equals(listDepartament.Getlist.ElementAt<i>, "Antioquia")
+            String line;
+            try
             {
+                StreamReader sr = new StreamReader(DEPARTAMENT);
 
+                line = "";
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    String[] info = line.Split(';');
+                    departament.Add(new ()
+                    {
+                        name = info[0],
+                        description = info[1],
+                        Email = info[2],
+                        Celphone = info[3]
+                    });
+
+                }
+
+                sr.Close();
+                //Console.ReadLine();
+                Thread.Sleep(4000);
             }
-        }*/
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
 
     }
 }
--- code/gui/DataReader.Designer.cs
+++ code/gui/DataReader.Designer.cs
@@ -264,6 +264,7 @@
             this.title.Size = new System.Drawing.Size(618, 24);
             this.title.TabIndex = 12;
             this.title.Text = "Data Requestment : Datos de la calidad del aire en Colombia";
+            this.title.Click += new System.EventHandler(this.title_Click);
             // 
             // specifiedvaluefordate
             // 
